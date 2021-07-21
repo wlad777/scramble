f9506e4ecf7aa17c5c8cd2aa8d8b2d274e0415c9
Waldemar <waldemar.falmann@gmail.com>
2021-07-21 18:46:44 +1200

    Add scramble? function

--- src/scramble/core.clj
+++ src/scramble/core.clj
@@ -1,8 +1,26 @@
 (ns scramble.core
   (:gen-class))
 
+
+(defn scramble?
+  "returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false"
+  [str1 str2]
+  (let [chars-map1 (frequencies (or str1 ""))
+        chars-map2 (frequencies (or str2 ""))]
+    (every? (fn [[c freq]]
+              (<= freq (get chars-map1 c 0)))
+            chars-map2)))
+
+
+#_(scramble? "rekqodlw" "world")
+#_(scramble? "cedewaraaossoqqyt" "codewars")
+#_(scramble? "katas" "steak")
+#_(scramble? "katas" "")
+#_(scramble? nil "steak")
+#_(scramble? "rekqodlw" nil)
+
+
 (defn -main [& args]
-  (prn args))
+  (scramble? (first args) (second args)))
 
 
-#_(-main "abc" 123)
--- test/scramble/core_test.clj
+++ test/scramble/core_test.clj
@@ -1,7 +1,29 @@
 (ns scramble.core-test
-  (:require [clojure.test :refer :all]
-            [scramble.core :refer :all]))
+  (:require [clojure.test :refer [deftest testing is are run-tests]]
+            [scramble.core :as x]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest scramble-test
+  (testing "should return true, if a portion of str1 characters can be rearranged to match str2"
+    (are [str1 str2] (is (x/scramble? str1 str2))
+      "rekqodlw" "world"
+      "rekqodlww" "world"
+      "cedewaraaossoqqyt" "codewars"
+      "cedewaraaossoqqyt" ""
+      "rekqodlw" nil))
+  
+  (testing "should return false, if a portion of str1 characters can't be rearranged to match str2"
+    (are [str1 str2] (is (not (x/scramble? str1 str2)))
+      "katas" "steak"
+      "" "steak"
+      nil "steak"
+      "rekqodlw" "wworld")))
+
+
+
+(comment
+
+  (run-tests)
+
+  (comment))
+
