42436ed6f3306bd3dcb27dc13cd4931559cae1b7
Waldemar <waldemar.falmann@gmail.com>
2021-07-22 18:57:26 +1200

    Create frontend app

--- cljs/scramble/events.cljs
+++ cljs/scramble/events.cljs
@@ -1,9 +1,47 @@
 (ns scramble.events
   (:require [re-frame.core :as re-frame]
-            [scramble.db :as db]))
+            [clojure.string :as s]
+            [ajax.core :as a]
+            [scramble.db :refer [default-db]]))
 
 
 (re-frame/reg-event-db
  ::initialize-db
  (fn [_ _]
-   db/default-db))
+   default-db))
+
+
+(re-frame/reg-event-db
+ ::scramble
+ (fn [db [_ params]]
+   (when-not (or (s/blank? (:str1 params))
+                 (s/blank? (:str2 params)))
+     (-> {:method          :get
+          :uri             (str "/scramble/" (:str1 params) "/" (:str2 params))
+          :format          (a/text-request-format)
+          :response-format (a/json-response-format)
+          :handler         #(if (first %)
+                              (re-frame/dispatch [:success-response (second %)])
+                              (re-frame/dispatch [:bad-response (second %)]))}
+         (a/ajax-request)))
+   db))
+
+
+(re-frame/reg-event-db
+ ::reset
+ (fn [db _]
+   (assoc db :result nil)))
+
+
+(re-frame/reg-event-db
+ :success-response
+ (fn [db [_ response]]
+   (assoc db :result response)))
+
+
+(re-frame/reg-event-db
+ :bad-response
+ (fn [db [_ err]]
+   (js/alert (str "Something went wrong: " (:status-text err)))
+   (assoc db :result nil)))
+
--- cljs/scramble/views.cljs
+++ cljs/scramble/views.cljs
@@ -1,24 +1,62 @@
 (ns scramble.views
-  (:require [reagent.core :as r]
+  (:require [clojure.string :as s]
+            [reagent.core :as r]
             [re-frame.core :as re-frame]
-            [scramble.subs :as subs]))
+            [scramble.subs :as subs]
+            [scramble.events :as events]))
 
 
-(def str1 (r/atom "rekqodlw"))
-(def str2 (r/atom "world"))
-
-
-(defn input-field [value]
+(defn- input-field [value]
   [:input
    {:type      "text"
     :value     @value
     :on-change #(reset! value (-> % .-target .-value))}])
 
 
+(defn- form [str1-atom str2-atom result-atom]
+  [:div
+   [:div.row
+    [:span.column
+     "Let's test, if a portion of this string: "]
+    [:div.column
+     [input-field str1-atom]]]
+   [:div.row
+    [:span.column
+     "can be rearranged to match this string: "]
+    [:div.column
+     [input-field str2-atom]]]
+   [:div.row
+    [:div.column]
+    [:div.column
+     [:button
+      {:disabled (or (s/blank? @str1-atom)
+                     (s/blank? @str2-atom)
+                     (not (nil? @result-atom)))
+       :on-click #(re-frame/dispatch [::events/scramble {:str1 @str1-atom
+                                                         :str2 @str2-atom}])}
+      "scramble"]]]])
+
+
+(defn- box [result]
+  (when-not (nil? result)
+    [:div.box
+     {:style {:border-color (if result "green" "maroon")
+              :background-color (if result "#aaeeaa" "#eeaaaa")}}
+     [:div.box-text
+      (if result
+        "Yes, it is possible to rearrange 🎉"
+        "Nope, some characters are missing 😟")]
+     [:div.box-button
+      [:button
+        {:on-click #(re-frame/dispatch [::events/reset])}
+       "close"]]]))
+
+
+(def str1 (r/atom "rekqodlw"))
+(def str2 (r/atom "world"))
+
 (defn main-panel []
   (let [result (re-frame/subscribe [::subs/result])]
-    [:div
-     [:h1 @result]
-     [input-field str1]
-     [input-field str2]
-     ]))
+    [:div.wrap
+     (box @result)
+     (form str1 str2 result)]))
